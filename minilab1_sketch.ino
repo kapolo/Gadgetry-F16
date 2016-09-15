9b877499259ade90c6c44d4df5a880917718d422
kapolo <kapolo@andrew.cmu.edu>
2016-09-15 17:25:26 -0400

    Add files via upload

---
+++ minilab1_sketch.ino
@@ -0,0 +1,27 @@
+int switchState = 0;//to capture switch state
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switchState = digitalRead(2);
+  //reads the state of the pin 0;
+
+  //control block
+  if(switchState == HIGH)
+  {
+    digitalWrite(13, LOW);
+    //if the state is high turn off LED
+    
+  }else
+  {
+    digitalWrite(13, HIGH);
+    //if the state is low turn on LED
+  }
+    
+}
